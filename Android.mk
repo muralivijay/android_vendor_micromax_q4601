27e9a141d61b5745d91d54ba727cc75c36c52309
muralivijay <muralidharan9845@gmail.com>
2020-04-10 19:26:08 +0530

    Q4601: Initial blobs

---
+++ Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter q4601, $(TARGET_DEVICE)),)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libaudiocustparam
+LOCAL_SRC_FILES_32 := vendor/lib/libaudiocustparam.so
+LOCAL_MULTILIB := 32
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+ 
+include $(CLEAR_VARS)
+LOCAL_MODULE = libmtkcam_fwkutils
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 = vendor/lib/libmtkcam_fwkutils.so
+include $(BUILD_PREBUILT)
+ 
+include $(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX = .so
+LOCAL_MULTILIB = 32
+LOCAL_SRC_FILES_32 = vendor/lib/libdpframework.so
+include $(BUILD_PREBUILT)
+
+endif
---
+++ q4601-vendor.mk
@@ -0,0 +1,17 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q4601/proprietary/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q4601/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q4601/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q4601/proprietary/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q4601/proprietary/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q4601/proprietary/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q4601/proprietary/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q4601/proprietary/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/q4601/proprietary/vendor/thh,system/vendor/thh)
